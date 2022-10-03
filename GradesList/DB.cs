902e056df5e848afde12e769bb1cd4d521191c53
Feligena <kiskatakizka@gmail.com>
2022-10-03 18:45:01 +0500

    изменения

--- GradesList/DB.cs
+++ GradesList/DB.cs
@@ -1,21 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DBConfigLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace GradesList
 {
     public class DB : DbContext
     {
         public DbSet<Student> TableStudents => Set<Student>();
-        public DbSet<AcademicDiscipline> TableAcademicDiscipline => Set<AcademicDiscipline>();
         public DbSet<Course> TableCourse => Set<Course>();
+        public DbSet<AcademicDiscipline> TableAcademicDiscipline => Set<AcademicDiscipline>();
+        public DbSet<GradeType> TableGreadType => Set<GradeType>();
 
         public DB()
         {
+            Database.EnsureDeleted(); //удаляет базу, если она существует
             Database.EnsureCreated(); //существует ли база, если нет - создает
         }
 
---
+++ GradesList/GradeType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesList
+{
+    [Table ("grade_type")]
+    public class GradeType //тип оценки, за что студент получил оценку
+    {
+        [Column("grade_type_id")]
+        public int gtID { get; set; }
+
+        [Column ("grade_type_name")]
+        [Required]
+        public string gtName { get; set; } //название типа оценки
+
+        public List<AcademicDiscipline> acadenicDisciplines = new List<AcademicDiscipline>(); //навигация в дисциплину
+    }
+}
